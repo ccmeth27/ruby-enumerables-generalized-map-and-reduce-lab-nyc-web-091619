699b9da8ec307d43affd459157d43059edf03e4d
Connor Meth <connormeth27@gmail.com>
2019-09-13 19:15:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,6 +4,7 @@ end
 map([1, 2, 3, -9]) {|n| n * -1}
 
 def reduce(array, starting_point = 0, &block)
+  if starting_point
   array.reduce(starting_point, &block)
 end
 reduce([1, 2, 3]) {|memo, n| memo + n}
