9eca73e88d48268bc6562e612d79f16d1087fa6d
Connor Meth <connormeth27@gmail.com>
2019-09-13 19:18:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,12 @@ def map(array, &block)
 end
 map([1, 2, 3, -9]) {|n| n * -1}
 
-def reduce(array, starting_point = 0, &block)
-  if starting_point
-  array.reduce(starting_point, &block)
+def reduce(array, starting_point = nil, &block)
+  if starting_point.nil?
+    array.reduce(&block)
+  else
+    array.reduce(starting_point, &block)
 end
-reduce([1, 2, 3]) {|memo, n| memo + n}
+reduce([1, 2, 3], 0) {|memo, n| memo + n}
 reduce([1, 2, 3], 100) {|memo, n| memo + n}
 reduce([false, nil, nil, nil, true]) {|memo, n| memo || n}
