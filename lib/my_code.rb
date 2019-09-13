730035ff8ff7389a21f08f138ab11bb1eb7bff62
Connor Meth <connormeth27@gmail.com>
2019-09-13 19:03:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,8 @@ def map(array, &block)
 end
 map([1, 2, 3, -9]) {|n| n * -1}
 
-def reduce(array, &block)
+def reduce(array, starting_point = 0, &block)
   array.reduce(&block)
 end
 reduce([1, 2, 3]) {|memo, n| memo + n}
+reduce([1, 2, 3], 100) {|memo, n| memo + n}
